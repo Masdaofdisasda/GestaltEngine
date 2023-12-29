45d4df7111642b8d93bf30eaa1faeba920d0fe62
Masdaofdisasda <david.koeppl75@gmail.com>
2023-12-29 14:38:24 +0100

    add clang and cmake formatter

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ import os
 import subprocess
 import configparser
 
-def create_vs_build(build_dir, cmake_path='..', vs_version='Visual Studio 17 2022', config_file='config.ini'):
+def create_vs_build(build_dir, cmake_path='..', vs_version='Visual Studio 17 2022', config_file='config.ini',  format_target='fix-format'):
     """
-    Creates a build directory and generates Visual Studio project files.
+    Creates a build directory, generates Visual Studio project files, and formats the code.
 
     :param build_dir: The directory where the build files will be created.
     :param cmake_path: The relative path to the CMakeLists.txt.
     :param vs_version: The Visual Studio generator version.
     :param config_file: Path to the configuration file.
+    :param format_target: The CMake target for formatting the code.
     """
 
     # Read the configuration file
@@ -34,6 +35,10 @@ def create_vs_build(build_dir, cmake_path='..', vs_version='Visual Studio 17 202
 
     # Execute the CMake command
     subprocess.run(cmake_command)
+    
+    # Run the format target
+    format_command = ['cmake', '--build', '.', '--target', format_target]
+    subprocess.run(format_command)
 
 if __name__ == '__main__':
     build_directory = 'project'
