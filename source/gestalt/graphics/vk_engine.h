d8ae96bd780b274f76eedca8136ee8ac79b20795
Masdaofdisasda <david.koeppl75@gmail.com>
2024-01-06 14:04:56 +0100

    finish vulkan initialization and present to surface

--- source/gestalt/graphics/vk_engine.h
+++ source/gestalt/graphics/vk_engine.h
@@ -5,6 +5,15 @@
 
 #include <vk_types.h>
 
+struct FrameData {
+  VkCommandPool _commandPool;
+  VkCommandBuffer _mainCommandBuffer;
+  VkSemaphore _swapchainSemaphore, _renderSemaphore;
+  VkFence _renderFence;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
 class VulkanEngine {
 public:
         VkInstance _instance;                       // Vulkan library handle
@@ -25,20 +34,20 @@ public:
         std::vector<VkImageView> _swapchainImageViews;
         VkExtent2D _swapchainExtent;
 
+        FrameData _frames[FRAME_OVERLAP];
+
+        VkQueue _graphicsQueue;
+        uint32_t _graphicsQueueFamily;
+
 	struct SDL_Window* _window{ nullptr };
 
 	static VulkanEngine& Get();
 
-	//initializes everything in the engine
+        FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
 	void init();
-
-	//shuts down the engine
 	void cleanup();
-
-	//draw loop
 	void draw();
-
-	//run main loop
 	void run();
 
   private:
