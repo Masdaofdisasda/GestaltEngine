960b91dd5b4bcbe7d4d182dc0e77745407e4eab3
Masdaofdisasda <david.koeppl75@gmail.com>
2024-01-20 15:06:07 +0100

    adapt Cmake for macOS with Clion

---
+++ setup_macos.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+import configparser
+
+def create_clion_build(build_dir, cmake_path='..', generator='Unix Makefiles', config_file='config.ini', format_target='fix-format'):
+    """
+    Creates a build directory, generates Unix Makefiles for CLion, and formats the code.
+
+    :param build_dir: The directory where the build files will be created.
+    :param cmake_path: The relative path to the CMakeLists.txt.
+    :param generator: The CMake generator to use.
+    :param config_file: Path to the configuration file.
+    :param format_target: The CMake target for formatting the code.
+    """
+
+    # Read the configuration file
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    vcpkg_toolchain_file = config.get('vcpkg', 'toolchain_file')
+
+    # Create the build directory if it doesn't exist
+    os.makedirs(build_dir, exist_ok=True)
+
+    # Construct the CMake command
+    # Example: cmake '..' -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE='path_to_vcpkg_toolchain'
+    cmake_command = [
+        'cmake',
+        cmake_path,
+        '-G', generator,
+        '-DCMAKE_TOOLCHAIN_FILE=' + vcpkg_toolchain_file  # Add vcpkg toolchain file
+    ]
+
+    # Change to the build directory
+    os.chdir(build_dir)
+
+    # Execute the CMake command
+    subprocess.run(cmake_command)
+    
+    # Run the format target
+    format_command = ['cmake', '--build', '.', '--target', format_target]
+    subprocess.run(format_command)
+
+if __name__ == '__main__':
+    build_directory = 'project'
+    create_clion_build(build_directory)
