40f7fffc1344a0c84733802bec21f4f477a61fe6
Masdaofdisasda <david.koeppl75@gmail.com>
2024-03-31 22:36:40 +0200

    added clang tidy

--- source/external/format.cmake
+++ source/external/format.cmake
@@ -14,3 +14,36 @@ set_property(TARGET fix-clang-format PROPERTY FOLDER "Formatting")
 set_property(TARGET fix-cmake-format PROPERTY FOLDER "Formatting")
 set_property(TARGET fix-format PROPERTY FOLDER "Formatting")
 set_property(TARGET format PROPERTY FOLDER "Formatting")
+
+
+file(GLOB_RECURSE ALL_SOURCE_FILES 
+    ${CMAKE_SOURCE_DIR}/source/gestalt/*.cpp
+    ${CMAKE_SOURCE_DIR}/source/gestalt/*.h
+    ${CMAKE_SOURCE_DIR}/source/gestalt/graphics/*.cpp
+    ${CMAKE_SOURCE_DIR}/source/gestaltgraphics/*.h
+)
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Custom target to run clang-tidy on all source files
+add_custom_target(
+    clang-tidy
+    COMMAND clang-tidy
+    ${ALL_SOURCE_FILES}
+    -p ${CMAKE_BINARY_DIR} 
+    -- 
+    # Add any additional clang-tidy options here, if needed.
+)
+
+add_custom_target(
+    fix-clang-tidy
+    COMMAND clang-tidy
+    -p ${CMAKE_BINARY_DIR}
+    -fix 
+    ${ALL_SOURCE_FILES}
+    --
+    # After '--', you can add additional arguments to pass to the compiler if needed
+)
+
+set_property(TARGET clang-tidy PROPERTY FOLDER "Linting")
+set_property(TARGET fix-clang-tidy PROPERTY FOLDER "Linting")
