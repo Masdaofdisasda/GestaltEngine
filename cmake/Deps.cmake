d6162bcba9783ccc24ecc096c87056f05eebd880
Masdaofdisasda <david.koeppl75@gmail.com>
2025-04-27 00:43:43 +0200

    restructure depenencies

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -1,3 +1,10 @@
+if(NOT EXISTS ${CMAKE_BINARY_DIR}/cmake/CPM.cmake)
+  file(DOWNLOAD https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/CPM.cmake
+       ${CMAKE_BINARY_DIR}/cmake/CPM.cmake
+  )
+endif()
+include(${CMAKE_BINARY_DIR}/cmake/CPM.cmake)
+
 # ------------ format ------------------------------
 CPMAddPackage(
   NAME Format.cmake
@@ -21,7 +28,7 @@ file(GLOB_RECURSE ALL_SOURCE_FILES
     ${CMAKE_SOURCE_DIR}/source/gestalt/*.cpp
     ${CMAKE_SOURCE_DIR}/source/gestalt/*.h
     ${CMAKE_SOURCE_DIR}/source/gestalt/graphics/*.cpp
-    ${CMAKE_SOURCE_DIR}/source/gestaltgraphics/*.h
+    ${CMAKE_SOURCE_DIR}/source/gestalt/graphics/*.h
 )
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
@@ -287,88 +294,41 @@ target_compile_definitions(ImGuizmo PRIVATE IMGUI_DEFINE_MATH_OPERATORS) # trigg
 set_property(TARGET ImGuizmo PROPERTY FOLDER "External/ImGUI")
 
 # ------------ jolt ------------------------------
-set(CMAKE_CONFIGURATION_TYPES "Debug;Release;Distribution")
 
-option(USE_STATIC_MSVC_RUNTIME_LIBRARY "Use static MSVC runtime library" OFF)
-
-# When turning this option on, the library will be compiled using doubles for positions. This allows for much bigger worlds.
-set(DOUBLE_PRECISION OFF)
-
-# When turning this option on, the library will be compiled with debug symbols
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(GENERATE_DEBUG_SYMBOLS ON)
+  set(GENERATE_DEBUG_SYMBOLS ON)
 else()
-    set(GENERATE_DEBUG_SYMBOLS OFF)
+  set(GENERATE_DEBUG_SYMBOLS OFF)
 endif()
 
-
-# When turning this option on, the library will be compiled in such a way to attempt to keep the simulation deterministic across platforms
-set(CROSS_PLATFORM_DETERMINISTIC OFF)
-
-# When turning this option on, the library will be compiled with interprocedural optimizations enabled, also known as link-time optimizations or link-time code generation.
-# Note that if you turn this on you need to use SET_INTERPROCEDURAL_OPTIMIZATION() or set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON) to enable LTO specifically for your own project as well.
-set(INTERPROCEDURAL_OPTIMIZATION ON)
-
-# Number of bits to use in ObjectLayer. Can be 16 or 32.
-set(OBJECT_LAYER_BITS 16)
-
-# Set X86 processor features to use, by default the library compiles with AVX2, if everything is off it will be SSE2 compatible.
-set(USE_SSE4_1 ON)
-set(USE_SSE4_2 ON)
-set(USE_AVX ON)
-set(USE_AVX2 ON)
-set(USE_AVX512 OFF)
-set(USE_LZCNT ON)
-set(USE_TZCNT ON)
-set(USE_F16C ON)
-set(USE_FMADD ON)
+set(USE_STATIC_MSVC_RUNTIME_LIBRARY OFF CACHE BOOL "Use static runtime library" FORCE)
 
 CPMAddPackage(
   NAME JoltPhysics
   GITHUB_REPOSITORY jrouwe/JoltPhysics
   GIT_TAG v5.1.0
   SOURCE_SUBDIR Build
-OPTIONS
-    "DOUBLE_PRECISION OFF"
-    "GENERATE_DEBUG_SYMBOLS ${GENERATE_DEBUG_SYMBOLS}"
-    "OVERRIDE_CXX_FLAGS OFF"
-    "CROSS_PLATFORM_DETERMINISTIC OFF"
-    "INTERPROCEDURAL_OPTIMIZATION ${INTERPROCEDURAL_OPTIMIZATION}"
-    "OBJECT_LAYER_BITS ${OBJECT_LAYER_BITS}"
-    "USE_SSE4_1 ${USE_SSE4_1}"
-    "USE_SSE4_2 ${USE_SSE4_2}"
-    "USE_AVX ${USE_AVX}"
-    "USE_AVX2 ${USE_AVX2}"
-    "USE_AVX512 ${USE_AVX512}"
-    "USE_LZCNT ${USE_LZCNT}"
-    "USE_TZCNT ${USE_TZCNT}"
-    "USE_F16C ${USE_F16C}"
-    "USE_FMADD ${USE_FMADD}"
-    "DEBUG_RENDERER_IN_DEBUG_AND_RELEASE OFF"
-    "DEBUG_RENDERER_IN_DISTRIBUTION OFF"
-    "PROFILER_IN_DEBUG_AND_RELEASE OFF"
-    "PROFILER_IN_DISTRIBUTION OFF"
-	"ENABLE_ALL_WARNINGS OFF"
+  OPTIONS
+        "DOUBLE_PRECISION OFF"
+        "GENERATE_DEBUG_SYMBOLS ${GENERATE_DEBUG_SYMBOLS}"
+        "OVERRIDE_CXX_FLAGS OFF"
+        "CROSS_PLATFORM_DETERMINISTIC OFF"
+        "INTERPROCEDURAL_OPTIMIZATION OFF"
+        "OBJECT_LAYER_BITS 16"
+        "USE_SSE4_1 ON"
+        "USE_SSE4_2 ON"
+        "USE_AVX ON"
+        "USE_AVX2 ON"
+        "USE_AVX512 OFF"
+        "USE_LZCNT ON"
+        "USE_TZCNT ON"
+        "USE_F16C ON"
+        "USE_FMADD ON"
+        "DEBUG_RENDERER_IN_DEBUG_AND_RELEASE OFF"
+        "DEBUG_RENDERER_IN_DISTRIBUTION OFF"
+        "PROFILER_IN_DEBUG_AND_RELEASE OFF"
+        "PROFILER_IN_DISTRIBUTION OFF"
+        "ENABLE_ALL_WARNINGS OFF"
 )
-
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-if (MSVC)
-    # Set MSVC runtime library for Debug and Release configurations
-    if (USE_STATIC_MSVC_RUNTIME_LIBRARY)
-        # Use static runtime libraries: /MT and /MTd
-        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-    else()
-        # Use dynamic runtime libraries: /MD and /MDd
-        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-    endif()
-endif()
-
-
-if (INTERPROCEDURAL_OPTIMIZATION)
-    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
-endif()
 
 set_property(TARGET Jolt PROPERTY FOLDER "External/")
