8093e3b2b2836e36535cfb48ca298c4030f58416
Masdaofdisasda <david.koeppl75@gmail.com>
2025-04-27 20:23:56 +0200

    simplify sdl

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -183,33 +183,25 @@ target_include_directories(vk-bootstrap PRIVATE "${Vulkan_INCLUDE_DIRS}")
 
 set_property(TARGET vk-bootstrap PROPERTY FOLDER "External/")
 
-# ------------ SDL2 ------------------------------
+# ───────────── SDL2 (static) ─────────────────────────────────────
 CPMAddPackage(
-  NAME SDL2
-  GITHUB_REPOSITORY libsdl-org/SDL
-  GIT_TAG release-2.30.1
-  OPTIONS "SDL2_DISABLE_INSTALL ON" "SDL_SHARED OFF" "SDL_STATIC ON" "SDL_STATIC_PIC ON"
-          "SDL_WERROR OFF"
+        NAME              SDL2
+        GITHUB_REPOSITORY libsdl-org/SDL
+        GIT_TAG           release-2.30.1
+        OPTIONS
+        "SDL_SHARED OFF"
+        "SDL_STATIC ON"
+        "SDL_TEST   OFF"
+        "SDL_INSTALL OFF"
+        "SDL_WERROR OFF"
 )
-file(GLOB SDL2_HEADERS "${SDL2_SOURCE_DIR}/include/*.h")
+add_library(Gestalt_SDL2 INTERFACE)
 
-add_custom_target(
-  sdl_copy_headers_in_build_dir
-  COMMAND ${CMAKE_COMMAND} -E copy_directory "${SDL2_SOURCE_DIR}/include"
-          "${CMAKE_BINARY_DIR}/SDLHeaders/SDL2"
-  DEPENDS ${SDL2_HEADERS}
-)
+target_link_libraries(Gestalt_SDL2 INTERFACE SDL2-static)
+target_include_directories(Gestalt_SDL2 INTERFACE ${SDL2_SOURCE_DIR}/include)
+target_compile_definitions(Gestalt_SDL2 INTERFACE $<$<PLATFORM_ID:Windows>:SDL_MAIN_HANDLED>)
 
-add_dependencies(SDL2-static sdl_copy_headers_in_build_dir)
-target_include_directories(SDL2-static INTERFACE "${CMAKE_BINARY_DIR}/SDLHeaders")
-# Define SDL_MAIN_HANDLED globally for SDL2-static
-target_compile_definitions(SDL2-static INTERFACE SDL_MAIN_HANDLED)
-
-set(SDL2_INCLUDE_DIR ${SDL2_SOURCE_DIR}/include)
-include_directories(${SDL2_INCLUDE_DIR})
-
-set_property(TARGET SDL2-static PROPERTY FOLDER "External/SDL2")
-set_property(TARGET sdl_copy_headers_in_build_dir PROPERTY FOLDER "External/SDL2")
+set_target_properties(SDL2-static PROPERTIES FOLDER "External/SDL2")
 set_property(TARGET sdl_headers_copy PROPERTY FOLDER "External/SDL2")
 set_property(TARGET SDL2_test PROPERTY FOLDER "External/SDL2")
 set_property(TARGET SDL2main PROPERTY FOLDER "External/SDL2")
@@ -220,7 +212,10 @@ CPMAddPackage(
   GITHUB_REPOSITORY jarikomppa/soloud
   GIT_TAG RELEASE_20200207
   SOURCE_SUBDIR contrib
-  OPTIONS "WITH_SDL2_STATIC ON" # Use SDL2 for audio backend
+  OPTIONS
+        "WITH_SDL2_STATIC ON"
+        "SDL2_INCLUDE_DIR ${SDL2_SOURCE_DIR}/include"
+        "SDL2_LIBRARY ${SDL2_STATIC_LIBRARY_PATH}"
 )
 
 set_property(TARGET soloud PROPERTY FOLDER "External")
