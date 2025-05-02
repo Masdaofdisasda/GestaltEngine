40d36ad9bb56e2911d17f0a3033fa3fc69d5dbd9
Masdaofdisasda <david.koeppl75@gmail.com>
2025-05-02 20:15:32 +0200

    fixed msv build

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -110,9 +110,11 @@ CPMAddPackage(
   NAME fmt
   GITHUB_REPOSITORY fmtlib/fmt
   GIT_TAG 10.2.1
-  OPTIONS "FMT_TEST OFF" "FMT_DOC OFF"
+        OPTIONS
+        "FMT_TEST OFF"
+        "FMT_DOC OFF"
+        "FMT_SHARED OFF"
 )
-target_compile_definitions(fmt INTERFACE FMT_SHARED=1)
 
 set_property(TARGET fmt PROPERTY FOLDER "External/")
 
